2d36906a04a9c820e5d0109dd5c2bcee720fb79e
ziniminisoldier <philipp.karst@gmx.de>
2023-05-12 10:20:01 +0200

    module: multiplication a * b |

--- script_01.js
+++ script_01.js
@@ -24,16 +24,23 @@ function add(a,b)
 }
 
 // module: subtraction a - b |
-output(subtract(3,2));
-output(subtract(3,-2));
-output(subtract(3,0));
+// output(subtract(3,2));
+// output(subtract(3,-2));
+// output(subtract(3,0));
 function subtract(a,b) 
 {
 	return a - b;
 }
 
+// module: multiplication a * b |
+output(multiply(3,2));
+output(multiply(3,-2));
+output(multiply(3,0));
 
-
+function multiply(a,b) 
+{
+	return a * b ;
+}
 
 // module: output | test:
 // output("hello");
